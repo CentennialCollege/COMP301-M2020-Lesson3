aeb22c3b38fc4f24955840c0b38c2041523dfaed
Tom <tom@localhost.localdomain>
2020-06-08 17:42:21 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("Tom's App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
